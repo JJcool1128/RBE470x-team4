730cc73713f405b49c4a0a23ff91727b9a98497d
Prahladh Raja <pnamboorkrishnam@wpi.edu>
2025-02-08 00:26:03 -0500

    added neighbors of 8 and is cell walkable for variant 2

--- teamNN/testcharacter.py
+++ teamNN/testcharacter.py
@@ -55,10 +55,6 @@ class TestCharacter(CharacterEntity):
             (0, -1),  # down
             (1, 0),   # right
             (-1, 0)  # left
-            # (1, 1),   # diagonal bottom-right
-            # (-1, -1), # diagonal top-left
-            # (1, -1),  # diagonal bottom-left
-            # (-1, 1)   # diagonal top-right
         ]
         #print(f"Current: {current}")
         for dx, dy in directions:
@@ -83,13 +79,17 @@ class TestCharacter(CharacterEntity):
         return abs(ax - bx) + abs(ay - by) # Manhattan distance
     
     print("cost")
-    def cost(self, a: tuple[int, int], b: tuple[int, int]) -> int:
+    def cost(self, wrld, a: tuple[int, int], b: tuple[int, int]) -> int:
         ax = a[0]
         ay = a[1]
         bx = b[0]
         by = b[1]
 
         final_cost = sqrt(pow(ax-bx, 2) + pow(ay-by, 2)) # Euclidean distance
+
+        if wrld.mons_at(b[0], b[1]) is not None and len(wrld.mons_at(b[0], b[1])) > 0:
+            return final_cost + 1
+
         return final_cost 
 
     print("astar")
@@ -110,8 +110,8 @@ class TestCharacter(CharacterEntity):
             if current == goal:
                 break
 
-            for next in self.find_neighbors(wrld, current):
-                new_cost = cost_so_far[current] + self.cost(current, next)
+            for next in self.neighbors_of_8(wrld, current):
+                new_cost = cost_so_far[current] + self.cost(wrld, current, next)
                 if next not in cost_so_far or new_cost < cost_so_far[next]:
                     cost_so_far[next] = new_cost
                     priority = new_cost + self.heuristic(goal, next)
@@ -125,7 +125,48 @@ class TestCharacter(CharacterEntity):
         path.reverse()  # Start -> Goal order
         return path
 
-           
+
+    def is_cell_walkable(self, wrld, a: tuple[int, int]) -> bool:
+        ax = a[0]
+        ay = a[1]
+
+        map_boundary_x = wrld.width()
+        map_boundary_y = wrld.height()
+        cell_walkable = True
+
+        if ax < 0 or ax >= map_boundary_x or ay < 0 or ay >= map_boundary_y:
+            cell_walkable = False
+        elif wrld.wall_at(ax, ay):
+            cell_walkable = False
+            
+        elif wrld.mons_at(ax, ay) is not None and len(wrld.mons_at(ax, ay)) > 0:
+            cell_walkable = False     
+
+        return cell_walkable
+        
+    
+    def neighbors_of_8(self, wrld, current: tuple[int, int]) -> list[tuple[int, int]]:
+        neighbors = []
+        a, b = current
+
+        directions = [
+            (0, 1),   # up
+            (0, -1),  # down
+            (1, 0),   # right
+            (-1, 0),  # left
+            (1, 1),   # diagonal bottom-right
+            (-1, -1), # diagonal top-left
+            (1, -1),  # diagonal bottom-left
+            (-1, 1)   # diagonal top-right
+        ]
+        #print(f"Current: {current}")
+        for dx, dy in directions:
+            nx, ny = a + dx, b + dy
+            if self.is_cell_walkable(wrld, nx, ny):
+                neighbors.append((nx, ny))
+
+        return neighbors
+
 
 
     
