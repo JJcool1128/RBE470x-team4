79bc957f66679ffdc6cbbbe359b9eaa24cf099a7
Prahladh Raja <pnamboorkrishnam@wpi.edu>
2025-03-08 02:06:03 -0500

    getting past 3rd wall when tested over 200-300 iterations

--- teamNN/project2/variant1.py
+++ teamNN/project2/variant1.py
@@ -1,5 +1,6 @@
 # This is necessary to find the main code
 import sys
+import matplotlib.pyplot as plt
 sys.path.insert(0, '../../bomberman')
 sys.path.insert(1, '..')
 
@@ -7,18 +8,30 @@ sys.path.insert(1, '..')
 from game import Game
 
 # TODO This is your code!
-sys.path.insert(1, '../teamNN')
-from testcharacter import TestCharacter
+reward_history = []
+for i in range(300):
+    sys.path.insert(1, '../teamNN')
+    from testcharacter import TestCharacter
 
 
-# Create the game
-g = Game.fromfile('map.txt')
+    # Create the game
+    g = Game.fromfile('map.txt')
 
-# TODO Add your character
-g.add_character(TestCharacter("me", # name
-                              "C",  # avatar
-                              0, 0  # position
-))
+    # TODO Add your character
+    test_character = TestCharacter("me", # name
+                                "C",  # avatar
+                                 0, 0)  # position
+    g.add_character(test_character)
+    #reward_history.append(sum(reward for (_, _, reward, _, _) in list(test_character.memory)[-30:]))
+
+    print("Iteration: ", i)
+    g.go(1)
+
+# plt.plot(reward_history)
+# plt.xlabel("Episode")
+# plt.ylabel("Total Reward")
+# plt.title("Reward Progression Over Episodes")
+# plt.show()
 
 # Run!
-g.go()
+#g.go()
